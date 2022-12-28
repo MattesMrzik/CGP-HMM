fixed error when scaling gradient wiht learning rate in manual training loop

cgp_hmm/CgpHmmCell.py
@@ -292,7 +292,7 @@ class CgpHmmCell(tf.keras.layers.Layer):
 
     def get_R(self, old_forward, init = False):
         if init:
-            return self.I_dense, 1.0 # bc return must be same in main and off branch, must be != 0 bc assert check z != 0
+            return self.I_dense, tf.cast(1.0, dtype = self.config.dtype) # bc return must be same in main and off branch, must be != 0 bc assert check z != 0
 
         if self.config.call_type in [0,2]: # A is sparse
             R = tf.sparse.sparse_dense_matmul(old_forward, self.A_sparse)
@@ -302,8 +302,8 @@ class CgpHmmCell(tf.keras.layers.Layer):
             R = tf.matmul(old_forward, self.A_dense)
 
         Z_i_minus_1 = tf.reduce_sum(old_forward, axis = 1, keepdims = True)
-        if add_epsilon_to_z:
-            Z_i_minus_1 = tf.math.add(Z_i_minus_1, add_epsilon_to_z)
+        # if add_epsilon_to_z:
+        #     Z_i_minus_1 = tf.math.add(Z_i_minus_1, add_epsilon_to_z)
         R /= Z_i_minus_1
         return R, Z_i_minus_1
 

cgp_hmm/Config.py
@@ -61,7 +61,7 @@ class Config():
 
         self.check_assert = not self.dont_check_assert
         self.generate_new_seqs = not self.dont_generate_new_seqs
-        self.dtype = tf.float64 if self.dytpe64 else tf.float32
+        self.dtype = tf.float64 if self.dtype64 else tf.float32
         self.learning_rate = self.learning_rate if not self.no_learning else 0
 
         self.gen_len = 3 * self.nCodons
@@ -151,7 +151,7 @@ class Config():
 
         # fine tune algo
         self.add_arg_main('--use_weights_for_consts', action='store_true', help ="use weights for transitions that become 1 after softmax")
-        self.add_arg_main('-d', '--dytpe64', action='store_true', help='using dytpe tf.float64')
+        self.add_arg_main('-d', '--dtype64', action='store_true', help='using dytpe tf.float64')
         self.add_arg_main('--clip_gradient_by_value', help ="clip_gradient_by_values", type = float)
         self.add_arg_main('--learning_rate', help ="learning_rate", type = float, default = 0.01)
         self.add_arg_main('--no_learning', help ="learning_rate is set to 0", action='store_true')

cgp_hmm/Training.py
@@ -236,8 +236,14 @@ def fit_model(config):
                     tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[0])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_I", summarize = config.assert_summarize)
                     tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[1])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_A", summarize = config.assert_summarize)
                     tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[2])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_B", summarize = config.assert_summarize)
+                    gradient[0] = config.learning_rate * gradient[0]
+                    gradient[1] = config.learning_rate * gradient[1]
+                    gradient[2] = config.learning_rate * gradient[2]
+                    tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[0])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_I_after_learning_rate_scaled", summarize = config.assert_summarize)
+                    tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[1])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_A_after_learning_rate_scaled", summarize = config.assert_summarize)
+                    tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(gradient[2])), [gradient[0], gradient[1], gradient[2]], name = "gradient_for_B_after_learning_rate_scaled", summarize = config.assert_summarize)
 
-                    optimizer.apply_gradients(zip(config.learning_rate * gradient, [layer.C.init_kernel, layer.C.transition_kernel, layer.C.emission_kernel]))
+                    optimizer.apply_gradients(zip(gradient, [layer.C.init_kernel, layer.C.transition_kernel, layer.C.emission_kernel]))
 
                     tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(layer.C.init_kernel)),       [layer.C.init_kernel, layer.C.transition_kernel, layer.C.emission_kernel], name = "I_kernel_after_apply_grads", summarize = config.assert_summarize)
                     tf.debugging.Assert(tf.math.reduce_all(tf.math.is_finite(layer.C.transition_kernel)), [layer.C.init_kernel, layer.C.transition_kernel, layer.C.emission_kernel], name = "A_kernel_after_apply_grads", summarize = config.assert_summarize)