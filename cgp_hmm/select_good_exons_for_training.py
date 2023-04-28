610185a1b9f6ee70d82638564adc91fd97d3cf9e
MattesMrzik <mattes@mrzik.de>
2023-04-28 17:24:15 +0200

    fixed bugs in select_good_exons_for_training

--- cgp_hmm/select_good_exons_for_training.py
+++ cgp_hmm/select_good_exons_for_training.py
@@ -22,24 +22,30 @@ def copy_to_new_good_exons_dir(df) -> None:
     create df with desired exons in interactive mode and pass it to this function
     this will create a new dir in the same dir as where args.p points to
     '''
-    dir_path = os.path.pardir(args.path_to_stats_table)
+    dir_path = os.path.dirname(args.path_to_stats_table)
+    print("dir_path", dir_path)
+    highest_id_for_good_exon_dir = 0
     for subdir in os.listdir(dir_path):
         # check if the subdirectory is a directory
         sub_path = os.path.join(dir_path, subdir)
-        highest_id_for_good_exon_dir = 0
         if x := re.search(r"good_exons.{0,1}(\d+)", subdir):
+            print("found prev good exon")
             highest_id_for_good_exon_dir = max(highest_id_for_good_exon_dir, int(x.group(1)))
 
-        new_good_exons_dir = f"{dir_path}/good_exons_{highest_id_for_good_exon_dir}"
-        os.makedirs(new_good_exons_dir)
-        for entry in df.iterrows():
-            exon_name = os.path.basename(entry["path"])
-            command = f"cp {dir_path}/{exon_name} {new_good_exons_dir}"
-            print(command)
-            # os.system(command)
+    highest_id_for_good_exon_dir += 1
 
-        # making the stats_table for the selected exons
-        sub_stats_table_path = f"{new_good_exons_dir}/stats_table.csv"
-        print("writing stats to", sub_stats_table_path, index=True, header=True, line_terminator='\n', sep=";")
-        df.to_csv(sub_stats_table_path)
+    new_good_exons_dir = f"{dir_path}/good_exons_{highest_id_for_good_exon_dir}"
+
+    os.makedirs(new_good_exons_dir)
+    for i, entry in df.iterrows():
+        print("entry", entry)
+        exon_name = os.path.basename(entry["path"])
+        command = f"cp -r {dir_path}/{exon_name} {new_good_exons_dir}"
+        print(command)
+        os.system(command)
+
+    # making the stats_table for the selected exons
+    sub_stats_table_path = f"{new_good_exons_dir}/stats_table.csv"
+    print("writing stats to", sub_stats_table_path)
+    df.to_csv(sub_stats_table_path, index=True, header=True, line_terminator='\n', sep=";")
 
